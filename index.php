b7fea07e58d3f798061471f4670717160d718fca
pierobramanti <piero.bramanti94@gmail.com>
2024-09-09 16:26:57 +0200

    [UPD] Installed Axios, Bootstrap, and Vue via CDN. Added array to JSON.

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <title>Php Todolist</title>
 </head>
 <body>
-    
+
+<div id="app"></div>
 </body>
 </html>
